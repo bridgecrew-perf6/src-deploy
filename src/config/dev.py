5e80c7f3b652d4ce1e0c157e9e70afa801b3be93
habib <habib.abdelgaber@yahoo.com>
2022-01-01 19:39:54 +0200

    7th commit

--- src/config/dev.py
+++ src/config/dev.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deploy-test-app.onrender.com']
 
 # Database
 DATABASES = {
