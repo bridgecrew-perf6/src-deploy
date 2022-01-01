7b2997807fc59e54c67d25466bea84dfffdd5200
habib <habib.abdelgaber@yahoo.com>
2022-01-01 19:34:35 +0200

    6th commit

--- src/config/dev.py
+++ src/config/dev.py
@@ -3,16 +3,14 @@ from decouple import config
 import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = 'django-insecure-#ozpdv!r#6e*2m=&#rg(txzy2+vnkgi9k-g6-4z399&i@$zty!'
+SECRET_KEY = config('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
